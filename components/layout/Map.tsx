1c095424d2ff9ac1d5439195eb1fcae81aa92437
axelio-dev <axel.barth@yahoo.com>
2025-10-22 19:00:01 +0200

    Modification : Installation de Mapbox au lieu de Maplibre

--- components/layout/Map.tsx
+++ components/layout/Map.tsx
@@ -1,6 +1,14 @@
-import { MapView } from "@maplibre/maplibre-react-native";
+import Mapbox from "@rnmapbox/maps";
 import React from "react";
 
+Mapbox.setAccessToken(null); // Aucune clé API
+
 export function Map() {
-  return <MapView style={{ flex: 1 }} />;
+  return (
+    <Mapbox.MapView
+      style={{ flex: 1 }}
+      styleURL="https://api.maptiler.com/maps/basic/style.json?key=ae4d8df1nN5qU23p6gvJ" 
+      // ✅ Style libre juste pour tester (clé publique gratuite de démonstration)
+    />
+  );
 }
