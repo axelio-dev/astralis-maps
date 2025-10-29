447f04e7c62042ef682ff1da5c5c99a02373d1d4
shadow-developper <axel.barth@yahoo.com>
2025-10-29 16:10:54 +0100

    App.tsx : Le LocationProvider enveloppe toute l'application

--- app.tsx
+++ app.tsx
@@ -2,15 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import HomeScreen from "./app/index";
+import { LocationProvider } from "./contexts/LocationContext";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <LocationProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LocationProvider>
   );
 }
